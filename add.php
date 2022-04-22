a61fbde9f7436a30ea79a705b3304f8890ae28d1
Jesús Ropero <sognol.developer@gmail.com>
2022-04-22 14:10:08 +0200

    create login & logout

--- add.php
+++ add.php
@@ -1,6 +1,11 @@
 <?php
   require "db.php";
 
+  if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    return;
+  }
+
   $error = null;
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
