a02de2b49e5691627b899a20ef7766b15bf45a0c
Jesús Ropero <sognol.developer@gmail.com>
2022-04-22 11:59:06 +0200

    create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,6 +4,16 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (255),
+    email VARCHAR (255) UNIQUE,
+    password VARCHAR (255)
+);
+
+INSERT INTO users (name, email, password) VALUES ("test1", "test1@gmail.com", "1234");
+
+
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (255),
@@ -11,3 +21,5 @@ CREATE TABLE contacts (
 );
 
 INSERT INTO contacts (name, phone_number) VALUES ("Paula", "123456789");
+INSERT INTO contacts (name, phone_number) VALUES ("Jesus", "999991289");
+INSERT INTO contacts (name, phone_number) VALUES ("Juan", "888991289");
